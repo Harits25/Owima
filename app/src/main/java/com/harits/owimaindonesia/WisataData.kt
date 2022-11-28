6a0be780f953ca4a8b0f3510e8a25b95f4e216c7
Harits25 <excialfh@gmail.com>
2022-11-28 10:01:42 +0700

    Initial commit

---
+++ app/src/main/java/com/harits/owimaindonesia/WisataData.kt
@@ -0,0 +1,117 @@
+package com.harits.owimaindonesia
+
+object WisataData {
+    private val wisataNames = arrayOf(
+        "Air Terjun Krecekan Denu",
+        "Air Terjun Seweru",
+        "Air Terjun Banyulawe Dong",
+        "Air Terjun Kucur",
+        "Gunung Wilis",
+        "Gunung Kendil",
+        "Waduk Bening Widas Saradan",
+        "Waduk Kedungbrubus",
+        "Waduk Notopuro",
+        "Waduk Dawuhan",
+    )
+    private val wisataDetails = arrayOf(
+        "Air Terjun Krecekan Denu, air terjun yang mulai populer pada tahun 2016 ini memiliki kondisi air yang bersih dan masih sangat alami serta juga belum banyak terjamah oleh banyak wisatawan.\n" +
+                "\n" +
+                "Tak hanya itu, disekitar air terjun juga memiliki suasana yang sangat segar karena adanya pepohonan yang lebat dan juga tebing-tebing yang curam yang semakin menambah keindahan air terjun.\n" +
+                "\n" +
+                "Bagi anda yang ingin mencari tempat untuk sekedar menyegarkan otak dan menghilangkan setress atas kepenatan sehari-hari, air terjun ini pas dijadikan alternatif untuk liburan akhir pekan anda.",
+        "Lokasi air terjun ini berada di atas ketinggian 400 mdpl. Oleh karena itu suasanan ayng tercipta di sekitar air terjun ini terasa sangat sejuk dan menenagkan. Sumber air dari air terjun ini berasal dar isekitaran kaki Gunung Wilis, yaitu keluar dari sela – sela pepohonan yang ada di Gunung Wilis.\n" +
+                "\n" +
+                "Di bawah air terjun juga terdapat kolam yang dibuat oleh para pengelola air terjun, sehingga relatif cukup aman untuk digunakan sebagai tempat berenang dan bermain air ataupun hanya sekedar bersantai di sekitaran kolam.",
+        "Seperti air terjun pada umumnya, disini pengunjung dapat merasakan nuansa alam yang masih sangat alami dan juga tenang. Tak hanya itu, letaknya yang berada di lereng pegunungan wilis membuat areal air terjun memiliki suasana yang sangat sejuk dan bersih.\n" +
+                "\n" +
+                "Fakta unik air terjun ini yaitu Air Terjun Banyulawe juga merupakan sasaran bagi para investor yang ingin berinvestasi dalam pembangkit tenaga listrik tenaga air,atau lebih mengembangkan bisnisnya dalam sektor pariwisata.",
+        "Air terjun dengan nama unik ini juga menjadi daya tarik tersendiri bagi para petualang. Anda dapat menikmati keindahan air terjun ini sesudah melewati pedesaan yang masih sangat asri. Tentunya perjalanan anda nantinya akan sangat berkesan dan menyenangkan.\n" +
+                "\n" +
+                "Sedikit berbeda dari air terjun lainnya, ketika anda sampai ke air terjun ini, anda akan dibuat kagum akan keindahan alamnya yang dihiasi oleh latar belakang perbukitan serta gunung wilis yang menjulang tinggi dan dijamin akan membuat anda enggan untuk beranjak dari tempat ini.",
+        "Dari tadi ngomongin Gunung Wilis, sebenarnya keindahan seperti apa sih yang dimiliki gunung ini? Gunung yang memiliki tinggi 2169 mdpl ini merupakan salah satu gunung non aktif yang ada di Jawa Timur. Luas gunung ini juga melingkupi 6 kabupaten yaitu Kediri, Madiun, Nganjuk, TulungAgung, Trenggalek, Ponorogo.",
+        "Pernah jalan-jalan ke Santa Monica, Los Angeles? Kalo belum pernah jangan khawatir, Indonesia juga punya kok lokasi wisata yang menawarkan pemandangan alam mirip Santa Monica, Los Angeles, tepatnya berada di Gunung Kendil, Jawa Timur.\n" +
+                "\n" +
+                "Dahulunya sebelum menjadi tempat wisata, tempat ini merupakan tempat latihan untuk para militer. Namun sekarang dialihfungsikan menjadi tempat wisata yang menarik. Dengan berbagai fasilitas pendukung yang berada disini, membuat siapa saja betah untuk berlama-lama menghabiskan waktu disini.\n",
+        "Tak hanya gunung dan air terjunnya saja yang menarik, ternyata Madiun juga memiliki waduk yang tak kalah indahnya dari objek wisata lainnya. Salah satu waduk yang ada di Madiun ialah Waduk Bening Widas.\n" +
+                "\n" +
+                "Waduk ini sering dijadikan sebagai tempat wisata bagi penduduk lokal, khusunya untuk para penduduk yang senang berwisata dengan keluarga dan bersantai tanpa harus terlalu jauh dari Madiun atau mengeluarkan uang banyak.\n" +
+                "\n" +
+                "Berbagai kegiatan dapat anda lakukan disirini seperti memancing, menaiki perahu, hingga berkemah pun bisa anda lakukan disini. Tak hanya itu, pemandangan berupa perahu-perahu berwarna warni juga menjadi daya tarik sendiri pada waduk ini dan pas untuk dijadikan objek foto yang indah dan tak terlupakan.",
+        "Selain Waduk Bening Widas, selanjutnya ada Waduk Kedungbrubus. Waduk ini membendung aliran sungai Kedungbrubus yang mempunyai debit air melimpah di musim penghujan yang berfungsi sebagai sarana irigasi warga setempat sekaligus objek wisata yang memikat.\n" +
+                "\n" +
+                "Akses jalannya yang mudah juga membuat para pengunjung berdatangan setiap harinya, ntah untuk memancing atau hanya sekedar bersantai sembari menikmati pemandangan hutan jati sepanjang 4 km. Uniknya lagi, disekitar waduk ini banyak sekali ditemukan fosil purba.\n",
+        "Waduk yang telah dialahfungsikan sebagai sarana rekreasi ini memiliki sifat waduk tadah hujan yang dapat menampung air dari resapan-resapan di sekitar hutan Lereng Gunung Pandan. Selain sebagai sarana rekreasi yang cukup menyenangkan, waduk ini juga dimanfaatkan sebagai arana irigasi dan budidaya ikan masyarakat setempat.\n" +
+                "\n" +
+                "Dan jika anda para penikmat memnacing, waduk ini juga memiliki kekayaan ikan air tawar di dalamnya, maka tak heran jika kebanyakan pengunjung merupakan pemancing yang datang dari luar kota maupun penduduk lokal itu sendiri.",
+        "Bingung mencari tempat wisata akhir pekan yang tenang dan kece bersama keluarga? Yuk datang aja ke Waduk Dawuhan, waduk ini memiliki suasana sejuk nan segar lengkap dengan pemandangan sekitar yang sangat tenang dan indah.\n" +
+                "\n" +
+                "Sama halnya dengan waduk lainnya, selain dijadikan tempat bersantai, waduk ini juga dimanfaatkan sebagai sarana irigasi. Dengan luas sekitar 1.273, setidaknya persawahan di 9 desa di 3 kecamatan terairi dari waduk ini."
+    )
+    private val wisataImage = intArrayOf(
+        R.drawable.airterjun_krecekandenu,
+        R.drawable.airterjun_seweru,
+        R.drawable.airterjun_banyulawedong,
+        R.drawable.airterjun_kucur_madiun,
+        R.drawable.gunung_wilis,
+        R.drawable.gunung_kendil,
+        R.drawable.waduk_bening_widas_saradan,
+        R.drawable.waduk_kedungbrubus,
+        R.drawable.waduk_notopuro,
+        R.drawable.waduk_dawuhan,
+    )
+    private val wisataRating = floatArrayOf(
+        4.4f, 4.4f, 4.5f, 4.2f, 4.4f, 4.2f, 4.1f, 4.0f, 4.4f, 4.1f
+    )
+    private val wisataHargaTiket = arrayOf(
+        "Rp 5.000",
+        "-",
+        "-",
+        "-",
+        "-",
+        "-",
+        "Rp 7.500",
+        "Rp 2.000",
+        "-",
+        "-"
+    )
+    private val wisataAlamat = arrayOf(
+        "Dusun Gilingan, Desa Kepel, Kecamatan Kare, Kabupaten Jember, Jawa Timur",
+        "Dusun Seweru, Desa Kare, Kecamatan Kare, Kabupaten Madiun, Jawa Timur",
+        "Dusun Gligi, Desa Kepel, kecamatan Kare, Kabupaten Madiun, Jawa Timur",
+        "Dusun Pucang Sawit, Desa Padas, Kecamatan Dagangan, Kabupaten Madiun, Jawa Timur",
+        "Gunung Wilis, Banaran, Pulung, Kabupaten Ponorogo, Jawa Timur",
+        "Area Sawah, Pilangrejo, Wungu, Madiun, Jawa Timur",
+        "Waduk Widas, Setren, Pajaran, Saradan, Madiun, Jawa Timur",
+        "Area Perhutani, Dawuhan, Pilangkenceng, Madiun, Jawa Timur",
+        "Desa Duren, Dusun Notopuro, Kecamatan Pilangkenceng, Kabupaten Madiun",
+        "Desa Sidomulyo, Kecamatan Wonoasri, Kabupaten Madiun, Jawa Timur"
+    )
+    private val wisataBuka = arrayOf(
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari (07.00 - 18.00 WIB)",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+        "Setiap Hari 24 Jam",
+    )
+    val listData: ArrayList<Wisata>
+        get() {
+            val list = arrayListOf<Wisata>()
+            for (position in wisataNames.indices) {
+                val wisata = Wisata()
+                wisata.name = wisataNames[position]
+                wisata.alamat = wisataAlamat[position]
+                wisata.buka = wisataBuka[position]
+                wisata.hargaTiket = wisataHargaTiket[position]
+                wisata.photo = wisataImage[position]
+                wisata.rating = wisataRating[position]
+                wisata.detail = wisataDetails[position]
+                list.add(wisata)
+            }
+            return list
+        }
+}
